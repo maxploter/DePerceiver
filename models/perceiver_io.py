209c1a2825a25ba96531cdeca6480772b45b301a
Maksim Ploter <maksim.ploter@gmail.com>
2024-07-25 14:48:24 +0300

    feat: add perceiver IO

---
+++ models/perceiver_io.py
@@ -0,0 +1,413 @@
+# MIT License
+#
+# Copyright (c) 2021 Phil Wang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Modifications copyright (C) 2024 Maksim Ploter
+
+from math import pi, log
+from functools import wraps
+
+import torch
+from torch import Tensor
+from torch import nn, einsum
+import torch.nn.functional as F
+
+from einops import rearrange, repeat
+from einops.layers.torch import Reduce
+
+
+from .backbone import build_backbone
+from .perceiver import ObjectDetectionHead, DePerceiver, fourier_encode
+from .detr import SetCriterion, PostProcess
+from .matcher import build_matcher
+from .segmentation import (DETRsegm, PostProcessPanoptic, PostProcessSegm,
+                           dice_loss, sigmoid_focal_loss)
+from util.misc import NestedTensor
+
+# helpers
+
+def exists(val):
+    return val is not None
+
+
+def default(val, d):
+    return val if exists(val) else d
+
+
+def cache_fn(f):
+    cache = None
+
+    @wraps(f)
+    def cached_fn(*args, _cache=True, **kwargs):
+        if not _cache:
+            return f(*args, **kwargs)
+        nonlocal cache
+        if cache is not None:
+            return cache
+        cache = f(*args, **kwargs)
+        return cache
+
+    return cached_fn
+
+
+# structured dropout, more effective than traditional attention dropouts
+
+def dropout_seq(seq, mask, dropout):
+    b, n, *_, device = *seq.shape, seq.device
+    logits = torch.randn(b, n, device=device)
+
+    if exists(mask):
+        logits = logits.masked_fill(~mask, -torch.finfo(logits.dtype).max)
+
+    keep_prob = 1. - dropout
+    num_keep = max(1, int(keep_prob * n))
+    keep_indices = logits.topk(num_keep, dim=1).indices
+
+    batch_indices = torch.arange(b, device=device)
+    batch_indices = rearrange(batch_indices, 'b -> b 1')
+
+    seq = seq[batch_indices, keep_indices]
+
+    if exists(mask):
+        seq_counts = mask.sum(dim=-1)
+        seq_keep_counts = torch.ceil(seq_counts * keep_prob).int()
+        keep_mask = torch.arange(num_keep, device=device) < rearrange(seq_keep_counts, 'b -> b 1')
+
+        mask = mask[batch_indices, keep_indices] & keep_mask
+
+    return seq, mask
+
+
+# helper classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn, context_dim=None):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.LayerNorm(dim)
+        self.norm_context = nn.LayerNorm(context_dim) if exists(context_dim) else None
+
+    def forward(self, x, **kwargs):
+        x = self.norm(x)
+
+        if exists(self.norm_context):
+            context = kwargs['context']
+            normed_context = self.norm_context(context)
+            kwargs.update(context=normed_context)
+
+        return self.fn(x, **kwargs)
+
+
+class GEGLU(nn.Module):
+    def forward(self, x):
+        x, gates = x.chunk(2, dim=-1)
+        return x * F.gelu(gates)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult=4):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * mult * 2),
+            GEGLU(),
+            nn.Linear(dim * mult, dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64):
+        super().__init__()
+        inner_dim = dim_head * heads
+        context_dim = default(context_dim, query_dim)
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+
+        self.to_q = nn.Linear(query_dim, inner_dim, bias=False)
+        self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias=False)
+        self.to_out = nn.Linear(inner_dim, query_dim)
+
+    def forward(self, x, context=None, mask=None):
+        h = self.heads
+
+        q = self.to_q(x)
+        context = default(context, x)
+        k, v = self.to_kv(context).chunk(2, dim=-1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+        if exists(mask):
+            mask = rearrange(mask, 'b ... -> b (...)')
+            max_neg_value = -torch.finfo(sim.dtype).max
+            mask = repeat(mask, 'b j -> (b h) () j', h=h)
+            sim.masked_fill_(~mask, max_neg_value)
+
+        # attention, what we cannot get enough of
+        attn = sim.softmax(dim=-1)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+        return self.to_out(out)
+
+
+# main class
+
+class PerceiverIO(nn.Module):
+    def __init__(
+            self,
+            *,
+            depth,
+            dim,
+            queries_dim,
+            logits_dim=None,
+            num_latents=512,
+            latent_dim=512,
+            cross_heads=1,
+            latent_heads=8,
+            cross_dim_head=64,
+            latent_dim_head=64,
+            weight_tie_layers=False,
+            decoder_ff=False,
+            seq_dropout_prob=0.
+    ):
+        for param_name, param_value in locals().items():
+            if param_name != 'self':
+                print(f"{param_name}: {param_value}")
+        super().__init__()
+        self.seq_dropout_prob = seq_dropout_prob
+
+        self.latents = nn.Parameter(torch.randn(num_latents, latent_dim))
+
+        self.cross_attend_blocks = nn.ModuleList([
+            PreNorm(latent_dim, Attention(latent_dim, dim, heads=cross_heads, dim_head=cross_dim_head),
+                    context_dim=dim),
+            PreNorm(latent_dim, FeedForward(latent_dim))
+        ])
+
+        get_latent_attn = lambda: PreNorm(latent_dim,
+                                          Attention(latent_dim, heads=latent_heads, dim_head=latent_dim_head))
+        get_latent_ff = lambda: PreNorm(latent_dim, FeedForward(latent_dim))
+        get_latent_attn, get_latent_ff = map(cache_fn, (get_latent_attn, get_latent_ff))
+
+        self.layers = nn.ModuleList([])
+        cache_args = {'_cache': weight_tie_layers}
+
+        for i in range(depth):
+            self.layers.append(nn.ModuleList([
+                get_latent_attn(**cache_args),
+                get_latent_ff(**cache_args)
+            ]))
+
+        self.decoder_cross_attn = PreNorm(queries_dim, Attention(queries_dim, latent_dim, heads=cross_heads,
+                                                                 dim_head=cross_dim_head), context_dim=latent_dim)
+        self.decoder_ff = PreNorm(queries_dim, FeedForward(queries_dim)) if decoder_ff else None
+
+        self.to_logits = nn.Linear(queries_dim, logits_dim) if exists(logits_dim) else nn.Identity()
+
+    def forward(
+            self,
+            data,
+            mask=None,
+            queries=None
+    ):
+        b, *_, device = *data.shape, data.device
+
+        x = repeat(self.latents, 'n d -> b n d', b=b)
+
+        cross_attn, cross_ff = self.cross_attend_blocks
+
+        # structured dropout (as done in perceiver AR https://arxiv.org/abs/2202.07765)
+
+        if self.training and self.seq_dropout_prob > 0.:
+            data, mask = dropout_seq(data, mask, self.seq_dropout_prob)
+
+        # cross attention only happens once for Perceiver IO
+
+        x = cross_attn(x, context=data, mask=mask) + x
+        x = cross_ff(x) + x
+
+        # layers
+
+        for self_attn, self_ff in self.layers:
+            x = self_attn(x) + x
+            x = self_ff(x) + x
+
+        if not exists(queries):
+            return x
+
+        # make sure queries contains batch dimension
+
+        if queries.ndim == 2:
+            queries = repeat(queries, 'n d -> b n d', b=b)
+
+        # cross attend from decoder queries to latents
+
+        latents = self.decoder_cross_attn(queries, context=x)
+
+        # optional decoder feedforward
+
+        if exists(self.decoder_ff):
+            latents = latents + self.decoder_ff(latents)
+
+        # final linear out
+
+        return self.to_logits(latents)
+
+
+def get_encode_fourier_features_fn(max_freq, num_freq_bands):
+    def encode_fourier_features(data):
+        b, *axis, _, device, dtype = *data.shape, data.device, data.dtype
+        # calculate fourier encoded positions in the range of [-1, 1], for all axis
+        axis_pos = list(map(lambda size: torch.linspace(-1., 1., steps=size, device=device, dtype=dtype), axis))
+        pos = torch.stack(torch.meshgrid(*axis_pos, indexing='ij'), dim=-1)
+        enc_pos = fourier_encode(pos, max_freq, num_freq_bands)
+        enc_pos = rearrange(enc_pos, '... n d -> ... (n d)')
+        enc_pos = repeat(enc_pos, '... -> b ...', b=b)
+        return enc_pos
+
+    return encode_fourier_features
+
+
+class DePerceiverIO(nn.Module):
+
+    def __init__(self, backbone, encode_fourier_features_fn, queries, perceiver_io, classification_head):
+        super().__init__()
+        self.backbone = backbone
+        self.perceiver_io = perceiver_io
+        self.classification_head = classification_head
+        self.queries = queries
+        self.encode_fourier_features_fn = encode_fourier_features_fn
+
+    def forward(self, samples: NestedTensor):
+        features = self.backbone(samples)
+        src, mask = [v for _, v in features.items()][-1].decompose()
+        src = src.permute(0, 2, 3, 1)
+        assert mask is not None
+
+        enc_pos = self.encode_fourier_features_fn(src)
+        src = torch.cat((src, enc_pos), dim=-1)
+
+        src = rearrange(src, 'b ... d -> b (...) d')
+
+        hs = self.perceiver_io(
+            data=src,
+            queries=self.queries,
+            mask=None,
+        )
+        out = self.classification_head(hs)
+        return out
+
+
+def build(args):
+    # the `num_classes` naming here is somewhat misleading.
+    # it indeed corresponds to `max_obj_id + 1`, where max_obj_id
+    # is the maximum id for a class in your dataset. For example,
+    # COCO has a max_obj_id of 90, so we pass `num_classes` to be 91.
+    # As another example, for a dataset that has a single class with id 1,
+    # you should pass `num_classes` to be 2 (max_obj_id + 1).
+    # For more details on this, check the following discussion
+    # https://github.com/facebookresearch/detr/issues/108#issuecomment-650269223
+    num_classes = 20 if args.dataset_file != 'coco' else 91
+    if args.dataset_file == "coco_panoptic":
+        # for panoptic, we just add a num_classes that is large enough to hold
+        # max_obj_id + 1, but the exact value doesn't really matter
+        num_classes = 250
+    device = torch.device(args.device)
+
+    backbone = build_backbone(args)[0] # TODO: hack to get backbone
+
+    num_freq_bands = 6  # number of freq bands, with original value (2 * K + 1)
+    max_freq = 10.  # maximum frequency, hyperparameter depending on how fine the data is
+    input_axis = 2
+    fourier_channels = (input_axis * ((num_freq_bands * 2) + 1))
+    input_dim = fourier_channels + backbone.num_channels
+
+    perceiver_io = PerceiverIO(
+        depth=args.self_per_cross_attn, # number of self attention blocks per cross attention
+        # (in fact  cross attention only happens once for Perceiver IO)
+        dim=input_dim, # number of channels for each token of the input
+        queries_dim=args.hidden_dim,
+        logits_dim=None, # NOT USED. output number of classes.
+        num_latents=args.num_queries, # number of latents or induced set points, or centroids. different papers giving it different names
+        latent_dim=args.hidden_dim, # latent dimension
+        cross_heads=1,
+        latent_heads=1,
+        cross_dim_head=backbone.num_channels, # number of dimensions per cross attention head
+        latent_dim_head=args.hidden_dim,
+        weight_tie_layers=False,  # whether to weight tie layers (optional, as indicated in the diagram)
+        decoder_ff=False,
+        seq_dropout_prob=0.
+    )
+
+    classifier_head = ObjectDetectionHead(
+        num_classes=num_classes,
+        num_latents=args.num_queries,
+        latent_dim=args.hidden_dim
+    )
+
+    queries = nn.Parameter(torch.randn(args.num_queries, args.hidden_dim))
+
+    model = DePerceiverIO(
+        backbone,
+        get_encode_fourier_features_fn(
+            max_freq=max_freq,
+            num_freq_bands=num_freq_bands,
+        ),
+        queries,
+        perceiver_io,
+        classifier_head,
+    )
+
+    if args.masks:
+        print("Using masks unsupported for perceiver model.")
+        raise NotImplementedError("Masks unsupported for perceiver")
+
+    matcher = build_matcher(args)
+    weight_dict = {'loss_ce': 1, 'loss_bbox': args.bbox_loss_coef}
+    weight_dict['loss_giou'] = args.giou_loss_coef
+    if args.masks:
+        weight_dict["loss_mask"] = args.mask_loss_coef
+        weight_dict["loss_dice"] = args.dice_loss_coef
+    # TODO this is a hack
+    if args.aux_loss:
+        aux_weight_dict = {}
+        for i in range(args.dec_layers - 1):
+            aux_weight_dict.update({k + f'_{i}': v for k, v in weight_dict.items()})
+        weight_dict.update(aux_weight_dict)
+
+    losses = ['labels', 'boxes', 'cardinality']
+    if args.masks:
+        losses += ["masks"]
+    criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
+                             eos_coef=args.eos_coef, losses=losses)
+    criterion.to(device)
+    postprocessors = {'bbox': PostProcess()}
+    if args.masks:
+        postprocessors['segm'] = PostProcessSegm()
+        if args.dataset_file == "coco_panoptic":
+            is_thing_map = {i: i <= 90 for i in range(201)}
+            postprocessors["panoptic"] = PostProcessPanoptic(is_thing_map, threshold=0.85)
+
+    return model, criterion, postprocessors
