2584dbda278d0b734a6fd4ba9f1bbd052c9eb7e1
Maksim Ploter <maksim.ploter@gmail.com>
2024-07-25 14:48:24 +0300

    feat: add perceiver IO

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
-from .perciever import build as build_perceiver
+from .perceiver import build as build_perceiver
+from .perceiver_io import build as build_perceiver_io
 
 
 def build_model(args):
     if args.model == 'perceiver':
         return build_perceiver(args)
+    elif args.model == 'perceiver_io':
+        return build_perceiver_io(args)
     else:
         return build(args)
